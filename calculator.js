2c0cf966d6a75395bbc7696bcaba845b9d352685
riyagh <riya60219@example.com>
2025-02-03 17:56:34 +0530

    add bmi calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res)=>{
-    res.send("<h1>Hi, World!</h1>");
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res)=>{
+
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1+num2;
+
+    res.send("The result of the calculation is " + result);
+})
+
+app.get("/bmicalculator", (req, res)=>{
+    res.sendFile(__dirname + "/bmicalculator.html");
+})
+
+app.post("/bmicalculator", (req, res)=>{
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+    let result = weight/(height*height);
+
+    res.send("Your BMI is " + result);
 })
 
 app.listen(3000, ()=>{
